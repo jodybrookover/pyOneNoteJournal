33e4ad9ed689b16dbe05169eb3e04fc0e0da3c88
Jody <jbrookover@legacyforhealth.org>
2013-11-23 17:43:11 -0500

    First working version

--- onepy.py
+++ onepy.py
@@ -1,27 +1,39 @@
 import json
 import win32com.client
 from xml.etree import ElementTree
+#from xml.etree.ElementTree import tostring
 
 onapp = win32com.client.gencache.EnsureDispatch('OneNote.Application')
-NS = "{http://schemas.microsoft.com/office/onenote/2010/onenote}"
+#TODO Make this variable based on your version of one note (/2010/, etc)
+NS = "{http://schemas.microsoft.com/office/onenote/2007/onenote}"
+
+
+#TODO Remove if not needed in the end
+class Section():
+    def __init__(self, id, type):
+        self.id = id
+        if type in ('section', 'sectiongroup'):
+            self.type = type
+        else:
+            raise ValueError("Type must be a value of 'section' or 'sectiongroup'")
 
 
 def getHierarchyJson():
     """Returns the Notebook Hierarchy as JSON"""
-    return(json.dumps(getHierarchy(), indent=4))
+    return(json.dumps(getNotebooks(), indent=4))
 
 
-def getHierarchy():
+def getNotebooks():
     """Returns the Notebook Hierarchy as a Dictionary Array"""
-    oneTree = ElementTree.fromstring(onapp.GetHierarchy("",win32com.client.constants.hsPages))
+    oneTree = ElementTree.fromstring(onapp.GetHierarchy("", win32com.client.constants.hsPages))
 
     notebooks = []
 
     for notebook in oneTree:
         nbk = parseAttributes(notebook)
-        if (notebook.getchildren()):
-           s, sg = getSections(notebook)
-           if (s != []):
+        if notebook.getchildren():
+           s, sg = _getSections(notebook)
+           if s != []:
                nbk['sections'] = s
 
            # Removes RecycleBin from SectionGroups and adds as a first class object
@@ -38,15 +50,47 @@ def getHierarchy():
     return notebooks
 
 
-def getSections(notebook):
+def getSectionsOfNotebook(notebook_id):
+    """Get a list of all the sections and section groups in the notebook, including section group children"""
+    oneTree = ElementTree.fromstring(onapp.GetHierarchy(notebook_id, win32com.client.constants.hsPages))
+    section_names = []
+
+    for section in oneTree:
+        nbk = notebook.attrib
+        section_name = nbk['name']
+        section_names.append(section_name)  # Includes section groups
+    return section_names
+
+
+def getSectionByName(notebook_id, section_name, section_type):
+    """Get a section by its name and type ("sectiongroup" or "section"
+    Returns a tuple (id, element) where element is an ElementTree Element"""
+    oneTree = ElementTree.fromstring(onapp.GetHierarchy(notebook_id, win32com.client.constants.hsPages))
+    lowercase_section_name = section_name.lower()  # For comparison inside a loop
+    target_section = None
+    for section in oneTree:
+        if section.attrib.get('name').lower() == lowercase_section_name:  # TODO Add check for type (group or not)
+            target_section = section
+            break   # Only get first find, if you have dupes, that's your problem
+    if target_section is None:
+        raise LookupError('Could not find Section with name: %s', section_name)
+    return target_section.attrib.get('ID'), target_section
+
+
+def createNewPage(section_id, title):
+    onapp.CreateNewPage(section_id)
+
+
+
+def _getSections(notebook):
     """Takes in a Notebook or SectionGroup  and returns a Dict Array of its Sections & Section Groups"""
     sections = []
     sectionGroups = []
     for section in notebook:
-        if (section.tag == NS + "SectionGroup"):
+        if section.tag == NS + "SectionGroup":
             newSectionGroup = parseAttributes(section)
-            if (section.getchildren()):
-               s, sg = getSections(section)
+            if section.getchildren():
+               s, sg = _getSections(section)
                if (sg != []):
                   newSectionGroup['sectionGroups'] = sg
                if (s != []):
@@ -84,12 +128,18 @@ def getMeta (page):
 def parseAttributes(obj):
     """Takes in an object and returns a dictionary of its values"""
     tempDict = {}
-    for key,value in obj.items():
+    for key, value in obj.items():
         tempDict[key] = value
     return tempDict
 
 
-def createNewPage(section_id):
-    """Creates a new page within the sectin provided"""
-    onapp.CreateNewPage(section_id)
+def createNewPage(section_id, page_title):
+    """Creates a new page within the section provided"""
+    new_page_id = onapp.CreateNewPage(section_id, win32com.client.constants.npsBlankPageWithTitle)
+    oneTree = ElementTree.fromstring(onapp.GetPageContent(new_page_id, win32com.client.constants.piAll))
+    xml = onapp.GetPageContent(new_page_id, win32com.client.constants.piAll)
+    # HACK ALERT!!
+    xml = xml.replace('<![CDATA[]]>', '<![CDATA[%s]]>' % page_title) # HACK!! I hate namespaces
+    xml = xml.replace('ns0:', 'one:')
+    onapp.UpdatePageContent(xml)
     return 1
--- pyOneNoteJournal.py
+++ pyOneNoteJournal.py
@@ -1,20 +1,24 @@
 #import win32com.client
 import onepy
 import datetime
+from sys import exit
+from pprint import pprint
 
 
 def find_journal_notebook():
-    notebook_name_guesses = ('work', 'Journal')  # Hopefully these are case insensitive
-    notebooks = onepy.getHierarchy()
+    """Look for a suitable notebook to use.
+    We guess it's named 'work' or 'journal'"""
+    notebook_name_guesses = ('work', 'journal')  # keep these lowercase since we convert to lowercase to match
+    notebooks = onepy.getNotebooks()
 
-    chosen_notebook = None
+    chosen_notebook_id = None
     for notebook in notebooks:
-        if notebook['name'] in notebook_name_guesses or notebook['nickname'] in notebook_name_guesses:
-            chosen_notebook = notebook['ID']
-            continue
-    if chosen_notebook is None:
+        if notebook['name'].lower() in notebook_name_guesses or notebook['nickname'] in notebook_name_guesses:
+            chosen_notebook_id = notebook['ID']
+            break
+    if chosen_notebook_id is None:
         return None
-    return chosen_notebook
+    return chosen_notebook_id
 
 
 def get_today():
@@ -22,22 +26,42 @@ def get_today():
     today = datetime.date.today()
     return today.year, today.strftime('%B'), today.day, today.isoformat()
 
-def main():
-#    onapp = win32com.client.Dispatch('OneNote.Application')
-#    win32com.client.gencache.EnsureDispatch('OneNote.Application')
 
-#    notebooks = onapp.GetHierarchy("", win32com.client.constants.hsNotebooks)
-#    notebooks_soup = BeautifulSoup(notebooks, 'lxml')
-#    print(notebooks_soup.prettify())
-    notebooks = onepy.getHierarchy()
-    print notebooks
-
-    chosen_notebook_id = find_journal_notebook()
-    onepy.getSections(chosen_notebook_id)
-    y,m,d,iso = get_today()
-    onepy.createNewPage()
+def create_year_section(journal_group_element, year):
+    #TODO use UpdateHierarchy method (ew)
     pass
 
+
+def section_exists(journal_group_element, section_name):
+    """Ensure the section exists within the given element"""
+    is_exists = False
+    for section in journal_group_element:
+        if section.attrib.get('name') == str(section_name):
+            is_exists = True
+    return is_exists
+
+
+def main():
+    #Final structure should look like this
+    #Work(or Journal) notebook -- 'Journal' section group -- YYYY section group -- MonthWord Section -- Date Page
+
+    target_notebook_id = find_journal_notebook()
+    target_journal_section_id, target_journal_section = onepy.getSectionByName(target_notebook_id, 'Journal', 'sectiongroup')
+    # Store some date related values we're gonna need
+    year, month_word, day, iso_format_date = get_today()
+
+    assert section_exists(target_journal_section, year), "No section group found for current year: %s" % year
+    year_section_id, year_section = onepy.getSectionByName(target_journal_section_id, str(year), 'sectiongroup')
+    assert section_exists(year_section, month_word), "No section group found for current month: %s" % month_word
+    month_section_id, month_section = onepy.getSectionByName(year_section_id, month_word, 'section')
+
+    month_pages = onepy.getPages(month_section)
+    for page in month_pages:
+        if page['name'] == iso_format_date:
+            print 'Page named "%s" already exists' % iso_format_date
+            exit()
+
+    onepy.createNewPage(month_section_id, iso_format_date)
 
 if __name__ == '__main__':
     main()
